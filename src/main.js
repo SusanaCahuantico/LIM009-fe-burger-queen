efcf3f7c4816688e16cc0ac212f423a3c33a268c
SusanaCahuantico <scahuantico@gmail.com>
2019-07-09 12:06:43 -0500

    configuracion firebase

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 // configuracion de firebase
+export const initFirebase = () => {
+    const firebaseConfig = {
+        apiKey: "AIzaSyBgWpkw-yFaHcElFT3Hu44zf8usV_FQ6l8",
+        authDomain: "burguer-queen-86f94.firebaseapp.com",
+        databaseURL: "https://burguer-queen-86f94.firebaseio.com",
+        projectId: "burguer-queen-86f94",
+        storageBucket: "",
+        messagingSenderId: "802259787846",
+        appId: "1:802259787846:web:32f4648600ef14ab"
+      };
+    firebase.initializeApp(firebaseConfig);
+    };
