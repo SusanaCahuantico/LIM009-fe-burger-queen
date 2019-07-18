663688b74f9dad1b604038ff23227a7770d6eb97
SusanaCahuantico <scahuantico@gmail.com>
2019-07-17 20:01:49 -0500

    Creamos el array de la lista de pedidos

--- src/view/order.js
+++ src/view/order.js
@@ -1,23 +1,33 @@
 import {WhereProduct} from '../controller/firebase.js'
 
+let array1 = [];
+
 export default () => {
     const createDiv = document.createElement('div');
     const order = `
+    <section class="col-lg-12">
     <header>
         <h1> BURGER QUEEN </h1>
     </header>
-    <section>
-        <button id="btn-desayuno"> Desayunar </button>
-        <button id="btn-ac"> Almuerzo y cena </button>
+    <section class="col-lg-6">
+        <button id="btn-desayuno" class="col-md-3"> Desayunar </button>
+        <button id="btn-ac" class="col-lg-3"> Almuerzo y cena </button>
+        <div id="contenido" class="col-lg-4">  </div>
     </section>
-    <div id="contenido">  </div>
-    <section>
-        <p> Nombre del cliente </p>
-        <input type="text" placeholder="Nombre del cliente"/>
-        <p> N° de mesa </p> 
-        <input type="text" placeholder="N° de mesa"/>
+    <section class="col-lg-6">
+        <p> Nombre del cliente <input type="text" name="nombre" required> </p>
+        <p> N° de mesa  <input type="number" name="nombre" required> </p> 
+        <div> 
+        <p> Cantidad </p>
+        <div id="cantidad"> </div>
+        <p> Productos </p>
         <div id="products"> </div>
-        <p> Total S/0.00 <p>
+        <p> Precio </p>
+        </div>
+        <div> <h1> Total S/ <h1> 
+        <p id="total"> </p>
+         </div>
+    </section>
     </section>
     `;
     createDiv.innerHTML = order;
@@ -44,10 +54,14 @@ export default () => {
                    createButton.id = `${element.id}`
                    contenido.appendChild(createButton);
                    createButton.addEventListener('click', ()=>{
-                       console.log(createButton.innerHTML)
+                       // console.log(createButton.innerHTML)
+                       const prodSelec = createButton.id;
                        const create = createButton.innerHTML;
+                       //products.innerHTML += create;
                        localStorage.setItem('producto', create)
-                       products.innerHTML += localStorage.getItem('producto', create)
+                       products.innerHTML += `<div> <input type="checkbox"/> <p> ${localStorage.getItem('producto', create)} <p> <p>  ${element.datos.Precio} <p> </div> `;  
+                       array1.push(prodSelec);
+                       console.log(array1);
                    })
             });     
     })
@@ -65,15 +79,32 @@ export default () => {
                 }); 
             })
             // return array;
-                //console.log(array);
+            //console.log(array);
                 const contenido = document.querySelector('#contenido');
-                //contenido.innerHTML = '';
+                const products = document.querySelector('#products')
+                contenido.innerHTML = '';
                 array.forEach((element) => {
                     const createButton = document.createElement('button');
                    createButton.innerHTML = `${element.datos.Nombre}`
+                   createButton.id = `${element.id}`
                    contenido.appendChild(createButton);
-            }); 
-    })
-});  
-    return createDiv;
-}
+                   createButton.addEventListener('click', ()=>{
+                      const prodSelec = createButton.id;
+                     // console.log(prodSelec)
+                      const create = createButton.innerHTML;
+                      //products.innerHTML += create;
+                      localStorage.setItem('producto', create)
+                      products.innerHTML += `<div> <input type="checkbox"/> <p> ${localStorage.getItem('producto', create)} <p> <p>  ${element.datos.Precio} <p> </div> `;  
+                      array1.push(prodSelec);
+                      console.log(array1);
+                    })
+                }); 
+            })
+        });
+        
+        return createDiv;
+    }
+    
+      let suma = 0;
+      suma = suma + parseInt(`${element.datos.Precio}`);
+      console.log(suma)
