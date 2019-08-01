5abd14785a8ea92e435ac37469cccf3f7891af4c
SusanaCahuantico <scahuantico@gmail.com>
2019-08-01 11:53:17 -0500

    implementando removeChild

--- src/view/home.js
+++ src/view/home.js
@@ -2,7 +2,7 @@ export default () => {
  const createDiv = document.createElement('div');
  const home = `
  <section>
-    <h1 class="burger col-12"> "Bienvenidos a Burger Queen" </h1>
+    <h1 class="burger col-12"> "Bienvenidos a" </h1>
     <img src="./image/burgerqueen.png" class="imgburger">
    <div class="divmesero">
      <a href='#/mesero' class="mesero col-4"> Mesero <a>
--- src/view/order.js
+++ src/view/order.js
@@ -1,6 +1,7 @@
 import { dataProduct } from '../controller/functions.js'
 
 let array1 = [];
+//let array2 = [];
 
 const createButton = (objproducto) => {
   //console.log(objproducto)
@@ -16,7 +17,7 @@ const createButton = (objproducto) => {
     createDiv.appendChild(createButton)
     createButton.addEventListener('click', (e)=>{
      const createBtn = e.target.id;
-    // if(createBtn){
+    console.log(createBtn)
        switch (createBtn){  
         case ('s3XmdNPPmSKupPjBj5IQ'): 
         case ('HYLEqOtNeTj3sEzBtabZ'):
@@ -30,32 +31,43 @@ const createButton = (objproducto) => {
         createBtnEle.id=createBtn+ele;
         createBtnEle.innerHTML = ele;
         //createBtnEle.className = "btnEle col-3";
-        //console.log(createBtnEle.id)
+        console.log(createBtnEle.id)
         div.appendChild(createBtnEle)
         createBtnEle.addEventListener('click', () => {
-        //const prodSelec = createButton.precio;
-        const prodSelecId = createBtn;
+        const prodSelec = createButton.precio;
       products.innerHTML +=
-      `<div> 
-      <li><input type="checkbox"/> <p> ${objproducto.datos.Nombre} de ${ele} ${objproducto.datos.Precio} <a id=""><img src="./image/delete-button.png"/></a></li> 
+      `<div id="removeProduct"> 
+      <li><input type="checkbox"/> <p> ${objproducto.datos.Nombre} de ${ele} ${objproducto.datos.Precio} <a id="remove-${createBtnEle.id}"><img src="./image/delete-button.png"/></a></li> 
       </div>`;  
-      array1.push(prodSelecId);
+      array1.push(prodSelec);
       const total = document.querySelector('#total');
+      const removeProduct = document.querySelector('#removeProduct')
       total.innerHTML = suma(array1)
-      localStorage.setItem( "Pedido", JSON.stringify(array1)) 
+      localStorage.setItem( "Pedido", JSON.stringify(array1))
+      const remove = document.querySelector(`#remove-${createBtnEle.id}`)
+      remove.addEventListener('click', () => {
+        removeProduct.removeChild(total)
+      })
           })    
          })
          break;
       default: 
-     // const prodSelec = createButton.precio;
-      const prodSelecId = createBtn;
-      products.innerHTML += `<div> 
-      <li><input type="checkbox"/> ${objproducto.datos.Nombre} ${objproducto.datos.Precio} <a id=""><img src="./image/delete-button.png"/></a></li> 
+     const prodSelec = createButton.precio;
+      //const prodSelec = createBtn;
+      //console.log(objproducto.id)
+      products.innerHTML += `<div id="removeProduct"> 
+      <li id="removeLi"><input type="checkbox"/> ${objproducto.datos.Nombre} ${objproducto.datos.Precio} <a id="remove-${objproducto.id}"><img src="./image/delete-button.png"/></a></li> 
       </div> `;  
-      array1.push(prodSelecId);
+      array1.push(prodSelec);
       const total = document.querySelector('#total');
+      const removeProduct = document.querySelector('#removeProduct')
+      const removeLi = document.querySelector('#removeLi')
       total.innerHTML = suma(array1)
       localStorage.setItem( "Pedido", JSON.stringify(array1)) 
+      const remove = document.querySelector(`#remove-${objproducto.id}`)
+      remove.addEventListener('click', () => {
+        removeProduct.removeChild(removeLi)
+      })
       break;
     } 
     })
@@ -101,23 +113,23 @@ export default () => {
     const order = `
     <section class="col-12">
     <header>
-    <h1> BURGER QUEEN </h1>
+    <h1 class="burgerTitulo"> BURGER QUEEN </h1>
     </header>
     <section class="col-6">
         <button id="btn-desayuno" class="col-6"> Desayuno </button>
         <button id="btn-ac" class="col-6"> Almuerzo y cena </button>
-        <div id="contenido" class="col-12">  </div>
+        <div id="contenido" class="col-12"> </div>
     </section>
     <section class="col-6">
-        <p> Nombre del cliente <input id="nombre" type="text" name="nombre" required> </p>
-        <p> N° de mesa  <input id="mesa" type="number" name="nombre" required> </p> 
-        <section> 
-           <div id="cantidad" class="col-md-4"> Cantidad </div>
-           <div id="products" class="col-md-4"> Productos </div>
-           <div class="col-4"> Precio </div>
+        <p class=""> Nombre del cliente: <input id="nombre" type="text" name="nombre" class="mesa" required> </p>
+        <p class=""> N° de mesa: <input id="mesa" type="number" name="nombre" class="mesa" required> </p>  
+        <section class="col-12"> 
+           <button class="productos col-md-4"> Cantidad </button>
+           <button class="productos col-md-4"> Productos </button>
+           <button class="productos col-4"> Precio </button>
+           <div id="products" class="col-12"></div>
         </div>
-        <div> <h1> Total S/ <h1> 
-        <p id="total"> </p>
+        <h1> Total S/ <p id="total"> </p> <h1>  
          </section>
     </section>
     </section>
