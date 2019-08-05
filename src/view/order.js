696fc8127b5c73b4c1be8e3fec2068c0a0f2a091
SusanaCahuantico <scahuantico@gmail.com>
2019-08-05 11:58:02 -0500

    capturando los valores input para enviarlos a firebase

--- src/view/order.js
+++ src/view/order.js
@@ -2,6 +2,7 @@ import { dataProduct } from '../controller/functions.js'
 
 let array1 = [];
 let array2 = [];
+//let arrayOrder = [];
 //aqui pintas el Storage: JSON.parseInt
 const array1Order = (objproducto, ele) => {
   if(objproducto.datos.Tipo != undefined){
@@ -26,6 +27,7 @@ const array1Order = (objproducto, ele) => {
 
 const createButton = (objproducto) => {
     const createDiv = document.createElement("div");
+    //console.log(createDiv)
     createDiv.id = 'div-add' + objproducto.id;
     const image = document.createElement('img');
     image.src = objproducto.datos.img;
@@ -33,24 +35,20 @@ const createButton = (objproducto) => {
     createDiv.appendChild(image);
     createDiv.className = "product col-3";
     const createButton = document.createElement("button");
-    //console.log(createButton)
     createButton.div = objproducto.datos.Tipo;
     createButton.innerHTML = objproducto.datos.Nombre;
     createButton.id = objproducto.id;
     createButton.precio = objproducto.datos.Precio;
-    //console.log(img)
     createDiv.appendChild(createButton)
     createButton.addEventListener('click', (e)=>{
       const createBtn = e.target.id;
+      const div = document.querySelector('#div-add'+ createBtn)
       //console.log(createBtn)
       //aqui pintas el Storage: JSON.parseInt
+     // div.innerHTML = '';
        switch (createBtn){  
         case ('s3XmdNPPmSKupPjBj5IQ'): 
         case ('HYLEqOtNeTj3sEzBtabZ'):
-          //console.log(objproducto.datos.Adicional)
-        const div = document.querySelector('#div-add'+ createBtn)    
-        const createDiv = document.createElement('div');
-        createDiv.id = createBtn; 
         (createButton.div).forEach(ele => {
           //res pollo y vegano
           const createBtnEle = document.createElement("button")
@@ -62,11 +60,10 @@ const createButton = (objproducto) => {
             array2.push(objproducto.datos.Precio)
             const total = document.querySelector('#total');
             total.innerHTML = suma(array2)
-            div.innerHTML = '';
-       products.innerHTML += 
-      `<div id="removeProduct"> 
-      <li><input type="checkbox"/> <p> ${objproducto.datos.Nombre} de ${ele} ${objproducto.datos.Precio} <a id="remove-${createBtnEle.id}"><img src="./image/delete-button.png"/></a></li> 
-      </div>`;  
+            products.innerHTML += 
+            `<div id="removeProduct"> 
+            <li><input type="checkbox"/>${objproducto.datos.Nombre} de ${ele} ${objproducto.datos.Precio} <a id="delete-${createBtnEle.id}"><img src="./image/delete-button.png"/></a></li> 
+            </div>`;  
           })    
          })
          const arrayAdic = objproducto.datos.Adicional;
@@ -77,9 +74,7 @@ const createButton = (objproducto) => {
             btnAdicional.innerHTML += `${elem.nombre}`;
             div.appendChild(btnAdicional)
             btnAdicional.addEventListener('click', () => {
-              console.log(elem.precio)
               array2.push(elem.precio)
-              const precioAdic = elem.precio;
               products.innerHTML += `${elem.nombre}`;
               const total = document.querySelector('#total');
               total.innerHTML = suma(array2)
@@ -88,27 +83,19 @@ const createButton = (objproducto) => {
          break;
       default: 
      array1Order(objproducto)
-      //const prodSelec = createButton.precio;
-      //console.log(array1)
+    // console.log(objproducto.id)
       array2.push(objproducto.datos.Precio)
     const total = document.querySelector('#total');
     total.innerHTML = suma(array2)
-    //console.log(array2)
-      //console.log(array1)
       products.innerHTML += `<div id="removeProduct"> 
-      <li id="removeLi"><input type="checkbox"/> ${objproducto.datos.Nombre} ${objproducto.datos.Precio} <a id="remove-${objproducto.id}"><img src="./image/delete-button.png"/></a></li> 
+      <li id="removeLi"><input type="checkbox"/> ${objproducto.datos.Nombre} ${objproducto.datos.Precio} <a id="delet-${objproducto.id}"> <img src="./image/delete-button.png"/> </a> </li> 
       </div> `;  
-      //array1.push(prodSelec);            
-     // const removeProduct = document.querySelector('#removeProduct')
-      //const removeLi = document.querySelector('#removeLi')
-      //localStorage.setItem( "Pedido", JSON.stringify(array1)) 
-      /*const remove = document.querySelector(`#remove-${objproducto.id}`)
-      remove.addEventListener('click', () => {
-        removeProduct.removeChild(removeLi)
-      })*/
-      break;
+     // const divRemove = document.querySelector('#removeProduct');
+      //const liRemove = document.querySelector('#removeLi');
+     // divRemove.removeChild(liRemove);
+     break;
     } 
-    })
+  })
     return createDiv;
 }
 
@@ -122,6 +109,7 @@ const suma = (arr) => {
 
 export default () => {
     const createDiv = document.createElement('div');
+    //console.log(createDiv)
     const order = `
     <section class="col-12">
     <header>
@@ -160,7 +148,7 @@ export default () => {
              arrayAc.forEach(element => {
                contenido.appendChild(createButton(element))
              //  console.log(createButton(element.datos.Nombre, element.id))
-             })
+             })             
           })
 //total.innerHTML = suma(array1)
        })
@@ -178,19 +166,11 @@ export default () => {
     })
   })
 
-   const nombre = createDiv.querySelector('#nombre')
-   nombre.addEventListener('click', () => {
-     console.log('hola soy nombre')
-   })
-
-   const mesa = createDiv.querySelector('#mesa')
-   mesa.addEventListener('click', () => {
-     console.log('mesa')
-   })
-
-   const enviar = createDiv.querySelector('#enviar')
-   enviar.addEventListener('click', () => {
-     console.log('Hola')
+  const enviar = createDiv.querySelector('#enviar')
+  enviar.addEventListener('click', () => {
+    const nombre = document.getElementById("nombre").value; 
+    const mesa = document.getElementById("mesa").value;
+     console.log(nombre, mesa)
    })
 
   return createDiv;
